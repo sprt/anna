5777b4375ff5dbab9ad5da8e802968dd5715091f
sprt <hellosprt@gmail.com>
2016-07-28 19:14:07 +0200

    Add command ping

---
+++ commands/ping.go
@@ -0,0 +1,19 @@
+package commands
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/sprt/anna"
+)
+
+func Ping(bot *anna.Bot, msg *discordgo.Message, args []string) error {
+	t, err := time.Parse("2006-01-02T15:04:05-07:00", msg.Timestamp)
+	if err != nil {
+		return err
+	}
+	say := fmt.Sprintf("Pong! (%.3fs)", time.Now().Sub(t).Seconds())
+	_, err = bot.Session.ChannelMessageSend(msg.ChannelID, say)
+	return err
+}
