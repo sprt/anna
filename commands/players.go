773d24be39077a42c09d24d1ac99c65eb1f3e2d4
sprt <hellosprt@gmail.com>
2016-08-04 19:29:46 +0200

    Sort output of !players all by game name

--- commands/players.go
+++ commands/players.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/boltdb/bolt"
@@ -20,23 +21,19 @@ func Players(bot *anna.Bot, msg *discordgo.Message, args []string) error {
 		}
 	}
 
+	var sp, mp []string
 	online, err := online(bot)
 	if err != nil {
 		return err
 	}
-
-	var sp, mp []string
-	for _, u := range online {
-		for _, pres := range u.Presences {
-			// TODO: use GameID (region-dependent)
-			// EU: CUSA00411_00
-			// US: CUSA00419_00
-			if pres.GameName == "Grand Theft Auto V" {
-				escaped := escape(u.Username)
-				if strings.Contains(pres.GameStatus, "Online") || strings.Contains(pres.GameStatus, "Heist") {
+	for _, gp := range online {
+		if gp.name == gtav {
+			for _, p := range gp.players {
+				escaped := escape(p.name)
+				if p.online {
 					mp = append(mp, escaped)
 				} else {
-					sp = append(sp, "*"+escaped+"*")
+					sp = append(sp, fmt.Sprintf("*%s*", escaped))
 				}
 			}
 		}
@@ -59,19 +56,13 @@ func playersAll(bot *anna.Bot, msg *discordgo.Message, args []string) error {
 		return err
 	}
 
-	byGame := make(map[string][]string)
-	for _, u := range online {
-		for _, pres := range u.Presences {
-			if pres.GameName != "" {
-				byGame[pres.GameName] = append(byGame[pres.GameName], u.Username)
-			}
-		}
-	}
-
 	var buf bytes.Buffer
-	buf.WriteString(fmt.Sprintf("%d players:\n", len(online)))
-	for game, users := range byGame {
-		buf.WriteString(fmt.Sprintf("⦁ **%s** — %s\n", game, strings.Join(users, ", ")))
+	for _, gp := range online {
+		var players []string
+		for _, p := range gp.players {
+			players = append(players, p.name)
+		}
+		buf.WriteString(fmt.Sprintf("⦁ **%s** — %s\n", gp.name, strings.Join(players, ", ")))
 	}
 
 	var say string
@@ -84,7 +75,7 @@ func playersAll(bot *anna.Bot, msg *discordgo.Message, args []string) error {
 	return err
 }
 
-func online(bot *anna.Bot) ([]*psn.User, error) {
+func online(bot *anna.Bot) ([]*gamePlayers, error) {
 	var online []*psn.User
 	if err := bot.DB.View(func(tx *bolt.Tx) error {
 		buk := tx.Bucket([]byte("default"))
@@ -97,5 +88,58 @@ func online(bot *anna.Bot) ([]*psn.User, error) {
 	}); err != nil {
 		return nil, err
 	}
-	return online, nil
+	return groupByGame(online), nil
 }
+
+func groupByGame(players []*psn.User) []*gamePlayers {
+	groupped := make(map[string][]*player)
+	for _, u := range players {
+		for _, pres := range u.Presences {
+			var online bool
+			switch pres.GameName {
+			case "":
+				continue
+			case gtav:
+				online = strings.Contains(pres.GameStatus, "Online") || strings.Contains(pres.GameStatus, "Heist")
+			}
+			groupped[pres.GameName] = append(groupped[pres.GameName], &player{name: u.Username, online: online})
+		}
+	}
+
+	var list []*gamePlayers
+	for game, players := range groupped {
+		list = append(list, &gamePlayers{name: game, players: players})
+	}
+
+	byGameName := byName(list)
+	sort.Sort(byGameName)
+	return byGameName
+}
+
+type player struct {
+	name   string
+	online bool
+}
+
+type gamePlayers struct {
+	name    string
+	players []*player
+}
+
+type byName []*gamePlayers
+
+func (p byName) Len() int      { return len(p) }
+func (p byName) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+func (p byName) Less(i, j int) bool {
+	switch {
+	case p[i].name == gtav:
+		return true
+	case p[j].name == gtav:
+		return false
+	default:
+		return p[i].name < p[j].name
+	}
+}
+
+const gtav = "Grand Theft Auto V"
